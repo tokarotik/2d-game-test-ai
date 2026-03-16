e8e9aea156af4a72de212f17bfdbd8774198773d
ToKarotik <markzhiv@gmail.com>
2026-03-16 21:26:39 +0200

    a little bit fixing

--- scenes/livers/player/player.gd
+++ scenes/livers/player/player.gd
@@ -1,21 +1,15 @@
 extends "res://scenes/livers/_base/_base.gd"
 
 @onready var camera = $Camera2D
-@onready var ui = $Control
-@onready var buttons_x = [$Control/left, $Control/right]
-@onready var buttons_y = [$Control/up, $Control/down]
 
 var SPEED = 250
 
 func _ready() -> void:
 	super._ready()
-	#ui.reparent(get_tree().get_root().get_children()[0])
 
 func _process(_e) -> void:
 	var input = get_keyboard_input()
-	#if input == Vector2.ZERO:
-	#	input = get_buttons_input()
-
+	
 	var vel = input * SPEED
 	velocity = vel
 	move_and_slide()
@@ -31,12 +25,3 @@ func get_keyboard_input() -> Vector2:
 	input.x = Input.get_axis("left", "right")
 	input.y = Input.get_axis("up", "down")
 	return input
-	
-func get_buttons_input() -> Vector2:
-	var input = Vector2.ZERO
-	input.x = get_buttons_axis(buttons_x[0], buttons_x[1])
-	input.y = get_buttons_axis(buttons_y[0], buttons_y[1])
-	return input
-
-func get_buttons_axis(button2: Button, button1: Button):
-	return type_convert(button1.button_pressed, TYPE_INT) - type_convert(button2.button_pressed, TYPE_INT)
--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 
 func _ready() -> void:
+	var os = OS.get_name().to_lower()
+
+	if os == 'android' or os == 'ios':
+		add_child(load("res://scenes/ui/touchscreen_input/touchscreen_input.tscn").instantiate())
 	
 	var camera_border = $camera_border.get_rect()
 	$Map.init(camera_border)
