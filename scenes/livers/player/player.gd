f600ec71b6e607b3f23422871f830a4bad07a230
ToKarotik <markzhiv@gmail.com>
2026-03-16 20:04:04 +0200

    added ui

--- scenes/livers/player/player.gd
+++ scenes/livers/player/player.gd
@@ -1,20 +1,44 @@
 extends "res://scenes/livers/_base/_base.gd"
 
 @onready var camera = $Camera2D
+@onready var ui = $Camera2D/Control
+@onready var buttons_x = [$Camera2D/Control/left, $Camera2D/Control/right]
+@onready var buttons_y = [$Camera2D/Control/up, $Camera2D/Control/down]
 
 var SPEED = 250
 
+func _ready() -> void:
+	ui.reparent(get_tree().get_root().get_children()[0])
+
 func _process(_e) -> void:
-	var input = Vector2.ZERO
-	input.x = Input.get_axis("left", "right")
-	input.y = Input.get_axis("up", "down")
+	var input = get_keyboard_input()
+	if input == Vector2.ZERO:
+		input = get_buttons_input()
 
 	var vel = input * SPEED
 	velocity = vel
 	move_and_slide()
+	
+	ui.position = global_position - (ui.get_rect().size / 2)
 
 func set_camera_borders(level_rect: Rect2) -> void:
+	return
 	camera.limit_left = level_rect.position.x
 	camera.limit_top = level_rect.position.y
 	camera.limit_right = level_rect.position.x + level_rect.size.x
 	camera.limit_bottom = level_rect.position.y + level_rect.size.y
+
+func get_keyboard_input() -> Vector2:
+	var input = Vector2.ZERO
+	input.x = Input.get_axis("left", "right")
+	input.y = Input.get_axis("up", "down")
+	return input
+	
+func get_buttons_input() -> Vector2:
+	var input = Vector2.ZERO
+	input.x = get_buttons_axis(buttons_x[0], buttons_x[1])
+	input.y = get_buttons_axis(buttons_y[0], buttons_y[1])
+	return input
+
+func get_buttons_axis(button2: Button, button1: Button):
+	return type_convert(button1.button_pressed, TYPE_INT) - type_convert(button2.button_pressed, TYPE_INT)
--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -2,5 +2,6 @@ extends Node2D
 
 
 func _ready() -> void:
+	
 	var camera_border = $camera_border.get_rect()
 	$Map.init(camera_border)
